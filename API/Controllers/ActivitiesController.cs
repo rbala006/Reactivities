d60883d0711e7433190c8e221c96f52cb8487b3d
bala <bala@teknomec.com>
2020-01-14 12:59:46 +0530

    stage 4 completed

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
 using MediatR;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -37,6 +36,20 @@ namespace API.Controllers
         {
             return await _mediator.Send(command);
 
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+
+            return await _mediator.Send(new Delete.Command{Id= id});
         }
     }
 }
